0205d4636cb04ffcdee929cd4006ee0bd5a4b927
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-09-10 05:06:54 -0400

    feat(auth): add /auth/oauth2callback to retrieve user info from Google after login

--- src/controllers/v1/auth.controllers.ts
+++ src/controllers/v1/auth.controllers.ts
@@ -1,6 +1,7 @@
 import prisma from "../../lib/dbConnection";
 import axios from "axios";
 import type { Request, Response } from "express";
+import { success } from "zod";
 
 export const googleAuth = async (req: Request, res: Response) => {
   try {
@@ -23,7 +24,48 @@ export const googleAuth = async (req: Request, res: Response) => {
 
 export const googleAuthCallback = async (req: Request, res: Response) => {
   try {
-    res.json({ message: "Google OAuth2 callback endpoint" });
+    const { code } = req.query;
+
+    if (!code) {
+      return res.status(400).json({
+        success: false,
+        message: "Authorization code is missing",
+        error: 400,
+        data: null,
+      });
+    }
+
+    const { data } = await axios.post("https://oauth2.googleapis.com/token", {
+      code,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+      grant_type: "authorization_code",
+    });
+
+    const { access_token, id_token, refresh_token } = data;
+
+    // Fetch user info
+    const userInfo = await axios.get(
+      `https://www.googleapis.com/oauth2/v3/userinfo`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Google authentication successful",
+      error: null,
+      data: {
+        user: userInfo.data,
+        access_token,
+        id_token,
+        refresh_token,
+      },
+    });
   } catch (error) {
     console.error("Error during Google OAuth2 callback:", error);
     res.status(500).json({ message: "Internal server error" });
