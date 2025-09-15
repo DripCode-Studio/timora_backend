c2fdcc5547129d7026cf33c6748a52f7f0b4c332
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-09-15 11:30:54 -0400

    Callback function enpoint implemented after a login'signup with google oauth

--- src/controllers/v1/auth.controllers.ts
+++ src/controllers/v1/auth.controllers.ts
@@ -1,7 +1,6 @@
 import prisma from "../../lib/dbConnection";
 import axios from "axios";
 import type { Request, Response } from "express";
-import { success } from "zod";
 
 export const googleAuth = async (req: Request, res: Response) => {
   try {
@@ -18,7 +17,7 @@ export const googleAuth = async (req: Request, res: Response) => {
     res.redirect(`${rootUrl}?${options.toString()}`);
   } catch (error) {
     console.error("Error during Google authentication:", error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error test" });
   }
 };
 
@@ -27,12 +26,7 @@ export const googleAuthCallback = async (req: Request, res: Response) => {
     const { code } = req.query;
 
     if (!code) {
-      return res.status(400).json({
-        success: false,
-        message: "Authorization code is missing",
-        error: 400,
-        data: null,
-      });
+      throw new Error("Authorization code is missing");
     }
 
     const { data } = await axios.post("https://oauth2.googleapis.com/token", {
@@ -55,19 +49,104 @@ export const googleAuthCallback = async (req: Request, res: Response) => {
       }
     );
 
-    res.status(200).json({
-      success: true,
-      message: "Google authentication successful",
-      error: null,
-      data: {
-        user: userInfo.data,
-        access_token,
-        id_token,
-        refresh_token,
+    const userExists = await prisma.users.findUnique({
+      where: {
+        email: userInfo.data.email,
       },
     });
+
+    // If user does not exist, create a new user else update the exiting user access token and refresh token (if refresh token is present)
+    if (!userExists) {
+      const newUser = await prisma.users.create({
+        data: {
+          email: userInfo.data.email,
+          role: "USER",
+          name: userInfo.data.name,
+          avatar: userInfo.data.picture,
+        },
+      });
+
+      if (!newUser) {
+        throw new Error("Failed to create new user");
+      }
+
+      if (newUser) {
+        const newAccount = await prisma.accounts.create({
+          data: {
+            userId: newUser.id,
+            googleId: userInfo.data.sub,
+            refresh_token: refresh_token,
+            access_token: access_token,
+          },
+        });
+
+        if (!newAccount) {
+          return res.status(500).json({
+            success: false,
+            message: "Failed to create new account",
+            error: 500,
+            data: null,
+          });
+        } else {
+          return res.status(200).json({
+            success: true,
+            message: "User created successfully",
+            error: null,
+            data: {
+              id: newUser.id,
+              email: newUser.email,
+              name: newUser.name,
+              role: newUser.role,
+            },
+          });
+        }
+      }
+    } else {
+      // update user account info in Account model
+      const updatedAccount = await prisma.accounts.updateMany({
+        where: {
+          userId: userExists.id,
+        },
+        data: {
+          access_token: access_token,
+          // only update refresh token if it is present in the response
+          ...(refresh_token && { refresh_token: refresh_token }),
+        },
+      });
+
+      if (updatedAccount.count === 0) {
+        return res.status(500).json({
+          success: false,
+          message: "Failed to update account",
+          error: 500,
+          data: null,
+        });
+      } else {
+        return res.status(200).json({
+          success: true,
+          message: "User logged in successfully",
+          error: null,
+          data: {
+            id: userExists.id,
+            email: userExists.email,
+            name: userExists.name,
+            role: userExists.role,
+          },
+        });
+      }
+    }
+
+    // creating user session later (better-auth or passport or JWT)
+
+    // send refresh_token in httpOnly cookie
+    // res.cookie("refresh_token", refresh_token, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === "production",
+    //   sameSite: "strict",
+    //   maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    // });
   } catch (error) {
     console.error("Error during Google OAuth2 callback:", error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error " + error });
   }
 };
--- src/server.ts
+++ src/server.ts
@@ -15,28 +15,34 @@ const allowedOrigins = [
   process.env.DEV_FRONTEND_URL || "http://localhost:5173",
   process.env.PROD_FRONTEND_ORIGIN || "https://yourdomain.com",
 ];
+// const corsOptions = {
+//   origin: (
+//     origin: string | undefined,
+//     callback: (err: Error | null, allow?: boolean) => void
+//   ) => {
+//     if (
+//       !origin ||
+//       allowedOrigins.includes(origin) ||
+//       /\.yourdomain\.com$/.test(origin)
+//     ) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
+
 const corsOptions = {
-  origin: (
-    origin: string | undefined,
-    callback: (err: Error | null, allow?: boolean) => void
-  ) => {
-    if (
-      !origin ||
-      allowedOrigins.includes(origin) ||
-      /\.yourdomain\.com$/.test(origin)
-    ) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
+  origin: "http://localhost:5173/",
+  // credentials: true,
   optionsSuccessStatus: 200,
 };
 
 // Middleware
-app.use(helmet());
-app.use(morgan("combined"));
+// app.use(helmet());
+app.use(morgan("dev"));
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
